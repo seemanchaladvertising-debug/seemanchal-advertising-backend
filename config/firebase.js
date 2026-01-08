44bbc12ac5d09c7e33860c3a9c7a6b92f646ecca
seemanchaladvertising <seemanchaladvertising@gmail.com>
2026-01-08 13:06:11 +0530

    "Add firebase admin config"
     firebase admin config

---
+++ config/firebase.js
@@ -0,0 +1,25 @@
+const admin = require("firebase-admin");
+
+if (!admin.apps.length) {
+  const projectId = process.env.FIREBASE_PROJECT_ID;
+  const clientEmail = process.env.FIREBASE_CLIENT_EMAIL;
+  const privateKey = process.env.FIREBASE_PRIVATE_KEY;
+
+  if (projectId && clientEmail && privateKey) {
+    admin.initializeApp({
+      credential: admin.credential.cert({
+        projectId,
+        clientEmail,
+        privateKey: privateKey.replace(/\\n/g, "\n"),
+      }),
+    });
+  } else {
+    admin.initializeApp();
+  }
+}
+
+const db = admin.firestore();
+
+console.log("🔥 Firebase Admin Initialized Successfully");
+
+module.exports = { admin, db };
